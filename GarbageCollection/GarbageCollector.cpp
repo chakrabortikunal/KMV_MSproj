3dab5288cdeca71a0a94923d76960f21eab97a8e
chakrabortikunal <chakrabortikunal@gmail.com>
2013-11-08 15:41:58 -0800

    1. Added the folder structure for Garbage Collector. 2. Added the folder structure for system include files

---
+++ GarbageCollection/GarbageCollector.cpp
@@ -0,0 +1,14 @@
+/*
+ * GarbageCollector.cpp
+ *
+ *  Created on: Nov 8, 2013
+ *      Author: K_Tiger
+ */
+
+#include "include.h"
+
+int GarbageCollecor(void)
+{
+	//wrtie logic here
+	return 1;
+}
---
+++ include/include.h
@@ -0,0 +1,15 @@
+/*
+ * include.h
+ *
+ *  Created on: Nov 8, 2013
+ *      Author: K_Tiger
+ */
+
+#ifndef INCLUDE_H_
+#define INCLUDE_H_
+
+#define USE_METADATA 1 //dummy declaration
+
+
+
+#endif /* INCLUDE_H_ */
